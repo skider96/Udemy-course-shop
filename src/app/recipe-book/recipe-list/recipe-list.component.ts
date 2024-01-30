696313475b5da26c512268ce70ccf3c43701ecbc
skider96 <danailqkiq@gmail.com>
2024-01-30 21:27:44 +0200

    studying 122th lesson

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,27 +1,22 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
-recipes: Recipe[] = [
-  new Recipe('A test Recipe', 'This is simply test', 
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/traditional-korean-dish-bibimbap-with-chopsticks-free-photo.jpg?w=2210&quality=70'),
-  new Recipe('Another test Recipe', 'This is simply test', 
-  'https://i0.wp.com/picjumbo.com/wp-content/uploads/traditional-korean-dish-bibimbap-with-chopsticks-free-photo.jpg?w=2210&quality=70'),
-]
-@Output() recipeWasSelected = new EventEmitter<Recipe>();
-  constructor() {
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
+  constructor(private recipeService: RecipeService) {
 }
 
 ngOnInit() {
-
+  this.recipes = this.recipeService.getRecipes();
 }
-
 
 onRecipeSelected(recipe: Recipe) {
 this.recipeWasSelected.emit(recipe);
---
+++ src/app/recipe-book/recipe.service.ts
@@ -0,0 +1,21 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is simply test',
+      'https://i0.wp.com/picjumbo.com/wp-content/uploads/traditional-korean-dish-bibimbap-with-chopsticks-free-photo.jpg?w=2210&quality=70'
+    ),
+    new Recipe(
+      'Another test Recipe',
+      'This is simply test',
+      'https://i0.wp.com/picjumbo.com/wp-content/uploads/traditional-korean-dish-bibimbap-with-chopsticks-free-photo.jpg?w=2210&quality=70'
+    ),
+  ];
+
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
