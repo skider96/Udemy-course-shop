03797f33253412c83e7c973c438a3b54621c9717
skider96 <danailqkiq@gmail.com>
2024-01-31 13:48:32 +0200

    studying 126th lesson

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
@@ -7,15 +8,21 @@ recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A test Recipe',
+      'Tasty Schnitzel',
       'This is simply test',
       'https://i0.wp.com/picjumbo.com/wp-content/uploads/traditional-korean-dish-bibimbap-with-chopsticks-free-photo.jpg?w=2210&quality=70'
-    ),
+    , [
+      new Ingredient('Meat', 1),
+      new Ingredient('French Fries', 20)
+    ]),
     new Recipe(
-      'Another test Recipe',
+      'Big Fat Burger',
       'This is simply test',
       'https://i0.wp.com/picjumbo.com/wp-content/uploads/traditional-korean-dish-bibimbap-with-chopsticks-free-photo.jpg?w=2210&quality=70'
-    ),
+    , [
+      new Ingredient('Buns', 2),
+      new Ingredient('Meat', 1)
+    ]),
   ];
 
 
