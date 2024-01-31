c5f1927d975eee94d89682aeb1f40a03d7465943
skider96 <danailqkiq@gmail.com>
2024-01-31 16:05:56 +0200

    studying 126th lesson

--- src/app/recipe-book/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,20 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css'
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit {
 @Input() recipe: Recipe;
+
+constructor (private recipeService: RecipeService) {}
+
+ngOnInit() {}
+
+onAddToShoppingList() {
+this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+}
 }
